b909663dd475bee9d64dc39115ec983eff5e5510
LYChubby <f4uzi345@gmail.com>
2025-06-27 23:45:53 +0700

    Menambah Logika Authorization Login

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -21,146 +21,162 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<LoginBloc, LoginState>(
-        listener: (context, state) {
-          if (state is LoginSuccess) {
-            // Navigator.pushReplacement(
-            //   context,
-            //   MaterialPageRoute(
-            //     builder: (context) =>
-            //         HomePage(namaPengguna: state.message.user?.name ?? 'User'),
-            //   ),
-            // );
-          } else if (state is LoginFailure) {
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(SnackBar(content: Text(state.error)));
-          }
-        },
-        builder: (context, state) {
-          return Form(
-            key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  const SizedBox(height: 60),
-                  Image.network(
-                    'https://static.vecteezy.com/system/resources/thumbnails/051/102/095/small_2x/pempek-an-amazing-indonesian-food-illustration-png.png',
-                    height: 200,
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Selamat Datang Kembali",
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 30),
-                  Text('Email'),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: 'Masukkan email',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      prefixIcon: const Icon(Icons.email),
+      body: SingleChildScrollView(
+        child: BlocConsumer<LoginBloc, LoginState>(
+          listener: (context, state) {
+            if (state is LoginSuccess) {
+              final role = state.responseModel.user?.role?.toLowerCase();
+              if (role == 'admin') {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (_) => AdminDashboardPage()),
+                  (route) => false,
+                );
+              } else if (role == 'user') {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (_) => MenuPage()),
+                  (route) => false,
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Role tidak dikenali")),
+                );
+              }
+            } else if (state is LoginFailure) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.error)));
+            }
+          },
+          builder: (context, state) {
+            return Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    const SizedBox(height: 60),
+                    Image.network(
+                      'https://static.vecteezy.com/system/resources/thumbnails/051/102/095/small_2x/pempek-an-amazing-indonesian-food-illustration-png.png',
+                      height: 200,
                     ),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'Email wajib diisi'
-                        : null,
-                  ),
-                  const SizedBox(height: 20),
-                  Text('Password'),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: _obscureText,
-                    decoration: InputDecoration(
-                      hintText: 'Masukkan password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      prefixIcon: const Icon(Icons.lock),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _obscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility,
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Selamat Datang Kembali",
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 30),
+                    Text('Email'),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: 'Masukkan email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                        onPressed: () {
-                          setState(() => _obscureText = !_obscureText);
-                        },
+                        prefixIcon: const Icon(Icons.email),
                       ),
+                      validator: (value) => (value == null || value.isEmpty)
+                          ? 'Email wajib diisi'
+                          : null,
                     ),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'Password wajib diisi'
-                        : null,
-                  ),
-                  const SizedBox(height: 30),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: const Color(0xFF582D1D),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                    const SizedBox(height: 20),
+                    Text('Password'),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        hintText: 'Masukkan password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                      ),
-                      onPressed: state is LoginLoading
-                          ? null
-                          : () {
-                              if (_formKey.currentState!.validate()) {
-                                final loginRequest = LoginRequestModel(
-                                  email: emailController.text,
-                                  password: passwordController.text,
-                                );
-                                context.read<LoginBloc>().add(
-                                  LoginRequested(requestModel: loginRequest),
-                                );
-                              }
-                            },
-                      child: state is LoginLoading
-                          ? const CircularProgressIndicator(color: Colors.white)
-                          : const Text("Masuk"),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Center(
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'Belum punya akun? ',
-                        style: const TextStyle(color: Colors.black),
-                        children: [
-                          TextSpan(
-                            text: 'Daftar Disini',
-                            style: const TextStyle(
-                              color: Color(0xFF582D1D),
-                              fontWeight: FontWeight.bold,
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const RegisterPage(),
-                                  ),
-                                );
-                              },
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
                           ),
-                        ],
+                          onPressed: () {
+                            setState(() => _obscureText = !_obscureText);
+                          },
+                        ),
+                      ),
+                      validator: (value) => (value == null || value.isEmpty)
+                          ? 'Password wajib diisi'
+                          : null,
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: const Color(0xFF582D1D),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: state is LoginLoading
+                            ? null
+                            : () {
+                                if (_formKey.currentState!.validate()) {
+                                  final loginRequest = LoginRequestModel(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                                  context.read<LoginBloc>().add(
+                                    LoginRequested(requestModel: loginRequest),
+                                  );
+                                }
+                              },
+                        child: state is LoginLoading
+                            ? const CircularProgressIndicator(
+                                color: Colors.white,
+                              )
+                            : const Text("Masuk"),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 20),
+                    Center(
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Belum punya akun? ',
+                          style: const TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: 'Daftar Disini',
+                              style: const TextStyle(
+                                color: Color(0xFF582D1D),
+                                fontWeight: FontWeight.bold,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegisterPage(),
+                                    ),
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }

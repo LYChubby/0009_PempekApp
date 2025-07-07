1c2b6f199df7cb4df3ebc12f9a3d2de5b07cce87
LYChubby <f4uzi345@gmail.com>
2025-07-08 04:32:53 +0700

    Hapus Camera Bloc

--- lib/data/services/service_http_client.dart
+++ lib/data/services/service_http_client.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ServiceHttpClient {
   final String baseurl =
-      'http://10.0.2.2:8000/api/'; // Replace with your actual base URL
+      'http://192.168.1.13:8000/api/'; // Replace with your actual base URL
   final secureStorage = FlutterSecureStorage();
 
   // POST
